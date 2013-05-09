a00d97542c75fd5dbc578e7fdd7469462d3ef241
Odin Hørthe Omdal <odinho@opera.com>
2013-05-09 11:25:26 +0200

    Upgrade CKEditor to 4.1.1

--- armstrong/hatband/static/ckeditor/config.js
+++ armstrong/hatband/static/ckeditor/config.js
@@ -1,12 +1,10 @@
-﻿CKEDITOR.editorConfig = function( config )
-{
-	config.toolbar = 'MyToolbar';
- 
-	config.toolbar_MyToolbar =
-	[
-		{ name: 'clipboard', items : [ 'PasteFromWord' ] },
-		{ name: 'styles', items : [ 'Format' ] },
-		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
-		{ name: 'links', items : [ 'Link','Unlink'] },
-	];
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
