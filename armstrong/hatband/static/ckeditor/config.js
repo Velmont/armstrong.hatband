c2554f9439e15496f90d3a4a16488344a41d1c8c
Odin Hørthe Omdal <odinho@opera.com>
2013-05-09 13:06:22 +0200

    Customizations for Hatband

--- armstrong/hatband/static/ckeditor/config.js
+++ armstrong/hatband/static/ckeditor/config.js
@@ -1,10 +1,27 @@
-/**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
- */
+﻿CKEDITOR.editorConfig = function( config )
+{
+	config.extraPlugins = 'autogrow';
+	config.toolbar = 'ShortToolbar';
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.toolbar_ShortToolbar =
+	[
+		{ name: 'clipboard', items : [ 'PasteFromWord' ] },
+		{ name: 'styles', items : [ 'Format' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+		{ name: 'links', items : [ 'Link','Unlink'] },
+	];
+
+    config.toolbar_FullToolbar = [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Scayt' ] },
+        { name: 'links', items: [ 'Link', 'Unlink' ] },
+        { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
+        { name: 'tools' },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] },
+        { name: 'others', items: [ '-' ] },
+        '/',
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
+        { name: 'styles', items: [ 'Styles', 'Format' ] },
+    ];
 };
